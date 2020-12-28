428c3c464cbf2aceda0cdf7756fab0722fd3cc55
mslater75 <54956427+mslater75@users.noreply.github.com>
2020-12-28 17:12:26 +0000

    Inital Commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
