22aa8b64caba32f2af05402fd5747d35a494dcfd
mslater75 <54956427+mslater75@users.noreply.github.com>
2021-01-01 20:54:54 +0000

    End of Section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,21 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Infrastructure.Data;
-using Core.Models;
 using Microsoft.EntityFrameworkCore;
-using Core.Interfaces;
 using AutoMapper;
 using API.Helpers;
+using API.Middleware;
+using API.Extensions;
 
 namespace API
 {
@@ -32,13 +24,15 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddScoped(typeof(IGenericRepository<>),(typeof(GenericRepository<>)));
-            services.AddScoped<IProductRepository, ProductRepository>();
+            
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddDbContext<StoreContext>(x => 
                 { 
                     x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
                 });
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+          
             /*services.AddDbContext<StoreContext>(x =>
             {
                 x.UseSqlite(_context.GetConnectionString("DefaultConnection"));
@@ -48,10 +42,15 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
+            }*/
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            //redirect to error controller
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
@@ -60,6 +59,8 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
+
+            app.UseSwaggerDocumentation();            
 
             app.UseEndpoints(endpoints =>
             {
