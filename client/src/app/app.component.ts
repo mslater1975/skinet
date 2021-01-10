955846bc62075c60a5c56c22dcc6e48ed7434e1d
mslater75 <54956427+mslater75@users.noreply.github.com>
2021-01-10 10:53:29 +0000

    Chapter 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+import { IPagination } from './shared/models/pagination';
+import { IProduct } from './shared/models/product';
 //import {HttpClient} from '@angular/common/http';
 
 @Component({
@@ -11,18 +11,11 @@ import { IProduct } from './models/product';
 })
 export class AppComponent implements OnInit {
   title = 'SkiNet';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {}
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: IPagination) => {
-      this.products = response.data;
-      //console.log(response);
-    }, error => {
-      console.log(error);
-    });
 
   }
 
