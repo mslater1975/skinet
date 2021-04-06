b86f3fe6445e8588cb14317d19a87d4f52b7fe0e
mslater75 <54956427+mslater75@users.noreply.github.com>
2021-04-06 10:13:43 +0100

    end of section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -100,6 +100,12 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = 
+                    new [] {"Email Address is in Use"}});
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
